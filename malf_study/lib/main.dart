34fc72c51c12c96d0a26a160ed9427895028c579
Hyunmin Cho <ggstargame45@koomin.ac.kr>
2023-07-18 13:51:07 +0900

    일단되는거

--- malf_study/lib/main.dart
+++ malf_study/lib/main.dart
@@ -1,139 +1,288 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(const MainApp());
-}
+Logger logger = Logger();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class UrlItem {
+  final int post_id;
+  final String title;
+  final String author_nickname;
+  final int author_nation;
+  final int user_type;
+  final int meeting_capacity;
+  final String meeting_pic;
+  final String meething_location;
+  final DateTime meeting_start_time;
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ListExample(),
+  UrlItem({
+    required this.post_id,
+    required this.title,
+    required this.author_nickname,
+    required this.author_nation,
+    required this.user_type,
+    required this.meeting_capacity,
+    required this.meeting_pic,
+    required this.meething_location,
+    required this.meeting_start_time,
+  });
+
+  factory UrlItem.fromJson(Map<String, dynamic> json) {
+    return UrlItem(
+      post_id: json['post_id'],
+      title: json['title'],
+      author_nickname: json['author_nickname'],
+      author_nation: json['author_nation'],
+      user_type: json['user_type'],
+      meeting_capacity: json['meeting_capacity'],
+      meeting_pic: "http://3.36.185.179:8000/${json['meeting_pic']}",
+      meething_location: json['meeting_location'],
+      meeting_start_time: DateTime.parse(json['meeting_start_time']),
     );
   }
 }
 
-class ListExample extends StatefulWidget {
-  const ListExample({super.key});
+class HttpProvider extends ChangeNotifier {
+  //List<PiscumPhotoModel> photos = [];
+  List<UrlItem> photos = [];
 
-  @override
-  State<ListExample> createState() => _ListExampleState();
-}
+  int currentPageNo = 1;
+  bool isAdd = false;
 
-class _ListExampleState extends State<ListExample> {
-  late List<String> _list;
-  late List<Image> _images;
-  late List<Container> _containers;
-  late int _listLength = 4;
-
-  void _resetlist() {
-    _listLength = 4;
-    _list = List<String>.generate(_listLength, (index) => 'Item $index');
-    _images = List<Image>.generate(_listLength,
-        (index) => Image.asset('assets/images/${Random().nextInt(4) + 1}.jpg'));
-    _containers = List<Container>.generate(
-        _listLength,
-        (index) => Container(
-              alignment: Alignment.topRight,
-              height: 200,
-              child: Row(children: [_images[index], Text(_list[index])]),
-            ));
+  Future<void> started() async {
+    await _getPhotos();
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _resetlist();
-  }
-
-  void _addlist() {
-    _listLength++;
-    _list.add('Item ${_listLength - 1}');
-    _images.add(Image.asset('assets/images/${Random().nextInt(4) + 1}.jpg'));
-    _containers.add(Container(
-      alignment: Alignment.topRight,
-      height: 200,
-      child: Row(
-        children: [_images[_listLength - 1], Text(_list[_listLength - 1])],
-      ),
-    ));
-  }
-
-  void _removelist() {
-    if (_listLength > 0) {
-      _listLength--;
-      _containers.removeLast();
-      _list.removeLast();
-      _images.removeLast();
+  void scrollListerner(ScrollUpdateNotification notification) {
+    if (notification.metrics.maxScrollExtent * 0.85 <
+        notification.metrics.pixels) {
+      _morePhotos();
     }
   }
-  //Image.asset('assets/images/${Random().nextInt(4) + 1}.jpg')
+
+  Future<void> _morePhotos() async {
+    if (!isAdd) {
+      isAdd = true;
+      notifyListeners();
+      List<UrlItem>? _data = await _fetchPost();
+      Future.delayed(const Duration(milliseconds: 1000), () {
+        photos.addAll(_data);
+        currentPageNo = currentPageNo + 1;
+        isAdd = false;
+        notifyListeners();
+      });
+    }
+  }
+
+  Future<void> _getPhotos() async {
+    List<UrlItem>? _data = await _fetchPost();
+    photos = _data;
+    currentPageNo = 2;
+    logger.e(currentPageNo);
+    notifyListeners();
+  }
+
+  Future<List<UrlItem>> _fetchPost() async {
+    try {
+      http.Response _response = await http
+          .get(Uri.parse("http://3.36.185.179:8000/bulletin-board/posts"));
+      if (_response.statusCode == 200) {
+        List<dynamic> _data = json.decode(_response.body)['data'];
+        List<UrlItem> _result = _data.map((e) => UrlItem.fromJson(e)).toList();
+        return _result;
+      } else {
+        return [];
+      }
+    } catch (error) {
+      logger.e(error);
+      return [];
+    }
+  }
+}
+
+AppBar appBar({
+  required String title,
+}) {
+  return AppBar(
+    title: Text(
+      title,
+      style: const TextStyle(fontSize: 18),
+    ),
+  );
+}
+
+class HttpWithHttpScreen extends StatelessWidget {
+  const HttpWithHttpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+    return MaterialApp(
+        home: ChangeNotifierProvider<HttpProvider>(
+            create: (_) => HttpProvider()..started(),
+            child: Consumer<HttpProvider>(builder: (context, state, child) {
+              return Scaffold(
+                  appBar: appBar(title: "Http"),
+                  body: NotificationListener<ScrollUpdateNotification>(
+                    onNotification: (ScrollUpdateNotification notification) {
+                      state.scrollListerner(notification);
+                      return false;
+                    },
+                    child: ListView.builder(
+                        itemCount: state.photos.length,
+                        itemBuilder: ((context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 8),
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.2,
+                                        height:
+                                            MediaQuery.of(context).size.width *
+                                                0.2,
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          child: Image.network(
+                                            state.photos[index].meeting_pic,
+                                            fit: BoxFit.cover,
+                                            frameBuilder: (BuildContext context,
+                                                Widget child,
+                                                int? frame,
+                                                bool wasSynchronouslyLoaded) {
+                                              return Container(
+                                                decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(12),
+                                                  color: const Color.fromRGBO(
+                                                      91, 91, 91, 1),
+                                                ),
+                                                child: child,
+                                              );
+                                            },
+                                            loadingBuilder:
+                                                (BuildContext context,
+                                                    Widget child,
+                                                    ImageChunkEvent?
+                                                        loadingProgress) {
+                                              if (loadingProgress == null) {
+                                                return child;
+                                              }
+                                              return Container(
+                                                decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(12),
+                                                  color: const Color.fromRGBO(
+                                                      91, 91, 91, 1),
+                                                ),
+                                                child: const Center(
+                                                  child:
+                                                      CircularProgressIndicator(
+                                                    color: Colors.amber,
+                                                  ),
+                                                ),
+                                              );
+                                            },
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(width: 12),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          _content(
+                                              url: state
+                                                  .photos[index].meeting_pic,
+                                              title: "ID : ",
+                                              content: state
+                                                  .photos[index].post_id
+                                                  .toString()),
+                                          _content(
+                                              url: state
+                                                  .photos[index].meeting_pic,
+                                              title: "Author : ",
+                                              content: state.photos[index]
+                                                  .author_nickname),
+                                          _content(
+                                              url: state
+                                                  .photos[index].meeting_pic,
+                                              title: "장소 : ",
+                                              content: "${20}"),
+                                          _content(
+                                              url: state
+                                                  .photos[index].meeting_pic,
+                                              title: " : ",
+                                              content: "${20}"),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                if (state.photos.length - 1 == index &&
+                                    state.isAdd) ...[
+                                  const SizedBox(
+                                    height: 100,
+                                    child: Center(
+                                        child: CircularProgressIndicator(
+                                      color: Colors.deepOrange,
+                                    )),
+                                  ),
+                                ],
+                              ],
+                            ),
+                          );
+                        })),
+                  ),
+                  floatingActionButton: FloatingActionButton(
+                    onPressed: () {},
+                    child: const Icon(Icons.add),
+                  ));
+            })));
+  }
+
+  GestureDetector _content({
+    required String title,
+    required String content,
+    required String url,
+  }) {
+    return GestureDetector(
+      onTap: () async {
+        if (await canLaunchUrlString(url)) {
+          await launchUrlString(url, mode: LaunchMode.externalApplication);
+        }
+      },
+      child: Row(
         children: [
-          SizedBox(
-            height: 700,
-            width: double.infinity,
-            child: Center(
-              child: ListView(
-                children: _containers,
-              ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _resetlist();
-                      });
-                    },
-                    child: const Icon(
-                      Icons.restart_alt,
-                      color: Colors.red,
-                      size: 30.0,
-                    )),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _addlist();
-                      });
-                    },
-                    child: const Icon(
-                      Icons.add,
-                      color: Colors.black,
-                      size: 30.0,
-                    )),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _removelist();
-                      });
-                    },
-                    child: const Icon(
-                      Icons.remove,
-                      color: Colors.yellow,
-                      size: 30.0,
-                    )),
-              ),
-            ],
-          )
+          Text(
+            content,
+            style: const TextStyle(
+                fontSize: 14, color: Color.fromRGBO(215, 215, 215, 1)),
+          ),
         ],
       ),
     );
   }
+}
+
+void main() {
+  //Provider.debugCheckInvalidValueType = null;
+  runApp(const HttpWithHttpScreen());
 }
